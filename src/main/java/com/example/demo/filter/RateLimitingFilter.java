aa8b2a1955b696f15776581477a6edf739e30cdf
Gaurav Arora <gaurav.arora0403@gmail.com>
2025-01-07 20:42:16 +0530

    added jwt-auth and rate limit filters

---
+++ src/main/java/com/example/demo/filter/RateLimitingFilter.java
@@ -0,0 +1,60 @@
+package com.example.demo.filter;
+
+import com.example.demo.models.RateLimitData;
+import com.example.demo.utils.RateLimitUtils;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Component
+public class RateLimitingFilter extends OncePerRequestFilter {
+
+    // Map to store request counts per IP address
+    private final Map<String, RateLimitData> requestCountsPerIpAddress = new ConcurrentHashMap<>();
+
+    // Maximum requests allowed per minute
+    private static final int MAX_REQUESTS_PER_MINUTE = 2;
+
+    private static final int MIN_IN_SECONDS_TO_RATE_LIMIT = 60;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+
+        String clientIpAddress = httpServletRequest.getRemoteAddr();
+
+        // Initialize request count for the client IP address
+        requestCountsPerIpAddress.putIfAbsent(clientIpAddress, RateLimitUtils.makeRateLimitData());
+        RateLimitData requestCount = requestCountsPerIpAddress.get(clientIpAddress);
+
+        // Increment the request count
+        int requests = requestCount.getRequestCounter().incrementAndGet();
+
+        // Check if the request limit has been exceeded
+        boolean ifPastTime = RateLimitUtils.checkIfPastTime(requestCount.getLastOccuringTimeStamp(), MIN_IN_SECONDS_TO_RATE_LIMIT);
+        if (requests > (MAX_REQUESTS_PER_MINUTE+1) && !ifPastTime) {
+            httpServletResponse.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+            httpServletResponse.getWriter().write("Too many requests. Please try again later.");
+            return;
+        } else {
+            requestCount.setLastOccuringTimeStamp(LocalDateTime.now());
+            if(ifPastTime) {
+                requestCount.setRequestCounter(new AtomicInteger(0));
+            }
+        }
+
+        // Allow the request to proceed
+        filterChain.doFilter(request, response);
+    }
+}
